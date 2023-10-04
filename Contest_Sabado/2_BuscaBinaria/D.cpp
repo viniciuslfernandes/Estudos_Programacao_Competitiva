169e7b2b9b123f968d5e7d12989120a6f36d40eb
Vinicius <viniciuslima99@live.com>
2023-10-04 11:29:38 -0300

    .

--- Contest_Sabado/2_BuscaBinaria/D.cpp
+++ Contest_Sabado/2_BuscaBinaria/D.cpp
@@ -1,55 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> vet;
-int buscaB(int a);
-pair<int, int> busca_soma(int x);
 int main(){
 
-    int n, r;
-    cin>>n>>r;
-    
-    for(int i=0; i<n; i++){
-        int x;cin>>x;
-        vet.push_back(x);
+    int n, x;
+    cin>>n>>x;
+    map<int, int> m;
+    for(int i=1; i<=n; i++){
+        int num;cin>>num;
+        if(m.find(num)==m.end()){
+            m[num]=i;
+        }
     }
-    pair<int ,int> x;
-    x=busca_soma(r);
-    if(x.first!=-1 && x.second !=-1){
-        cout<<x.first+1<<" "<<x.second+1<<endl;
-    }else{
-        cout<<"IMPOSSIBLE"<<endl;
+    for(auto it=m.begin(); it!=m.end(); it++){
+        int a = x - it->first;
+        if(m.find(a)!=m.end()){
+            if()
+        }
+
     }
+    //cout<<"IMPOSSIBLE"<<endl;
     return 0;
-}
-
-pair<int, int> busca_soma(int x){
-
-    for(int i=0; i<vet.size(); i++){
-        int a= x-vet[i];
-        a=buscaB(a);
-        if(a!=-1){
-            return make_pair(a, i);
-        }
-    }
-    return make_pair(-1, -1);
-}
-
-int buscaB(int a){
-
-    int ini = 0;
-    int fim = vet.size() - 1;
-
-    while (ini <= fim) {
-        int meio = ini + (fim - ini) / 2;
-
-        if (vet[meio] == a) {
-            return meio; 
-        } else if (vet[meio] < a) {
-            ini = meio + 1;
-        } else {
-            fim = meio - 1;
-        }
-    }
-
-    return -1; 
 }
--- Noic/6_TecnicasProgramacao/1_BuscaBinaria/4_BinarySearch.cpp
+++ Noic/6_TecnicasProgramacao/1_BuscaBinaria/4_BinarySearch.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int main(){
 
-    ll n, q; cin>>n>>q;
-    map<ll,ll> v; 
-    ll x;
-    for(ll i=0; i<n; i++){
-        cin>>x;
-        v.insert(make_pair(x,i));
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, q; cin>>n>>q;
+    map<int,int> v; 
+    
+    for(int i=0; i<n; i++){
+        int x; cin>>x;
+        if(v.find(x)==v.end()){
+            v[x]=i;
+        }
     }
     
-    for(ll i=0; i<q; i++){
-        cin>>x;
-        auto it=v.find(x);
-        if(it!=v.end()){
-            cout<<it->second<<endl;
+    for(int i=0; i<q; i++){
+        int a; cin>>a;
+        if(v.find(a)!=v.end()){
+            cout<<v[a]<<endl;
         }else{
             cout<<"-1"<<endl;
         }
