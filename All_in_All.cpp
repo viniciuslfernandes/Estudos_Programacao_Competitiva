3aaf134ad93a8470d1539f3a7502326a21a3392c
Vinicius <viniciuslima99@live.com>
2023-10-27 13:16:45 -0300

    a

---
+++ All_in_All.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string x, y;
+    while(cin>>x>>y){
+        for(int i=0; i<y.size(); i++){
+            
+        }
+    }
+    return 0;
+}
--- teste.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-
-    string str;
-    cin>>str;
-    cout<<str.size();
-    return 0;
-}
